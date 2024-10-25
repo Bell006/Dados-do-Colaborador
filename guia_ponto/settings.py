7e794dbc098caacfa6c5fe17f4f2804abbea0838
Bell006 <bellamancio006@gmail.com>
2024-10-25 16:53:24 -0300

    Setting up wsgi.py

--- guia_ponto/settings.py
+++ guia_ponto/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['dados-do-colaborador.onrender.com', 'onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
