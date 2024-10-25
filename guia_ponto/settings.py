cbbd74778398416c326989d9b7a976028cf2dade
Bell006 <bellamancio006@gmail.com>
2024-10-25 16:15:48 -0300

    Changing debg mode in production

--- guia_ponto/settings.py
+++ guia_ponto/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['dados-do-colaborador.onrender.com/',]
+ALLOWED_HOSTS = ['onrender.com',]
 
 
 # Application definition
