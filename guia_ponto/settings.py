0910fffa6163b3895f9a499c1fb00a63ee26cb3b
Bell006 <bellamancio006@gmail.com>
2024-10-25 16:50:12 -0300

    Setting up wsgi.py

--- guia_ponto/settings.py
+++ guia_ponto/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['onrender.com',]
+ALLOWED_HOSTS = ['onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -122,11 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
