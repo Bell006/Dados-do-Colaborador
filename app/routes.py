35b90cd5f8fa1d383c0f57a45c8b458e2d15d62d
Bell006 <bellamancio006@gmail.com>
2024-10-28 11:14:12 -0300

    Fixing Flask-Bootstrap import

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,14 @@
 from flask import render_template, request, redirect, flash, current_app
-
-# Remova a importação do bootstrap aqui
 from app.utils import add_to_google_sheet
 
-def index(bootstrap):  # Adicione o bootstrap como argumento
+def index():
     if request.method == 'POST':
         email = request.form.get('email')
         name = request.form.get('name')
 
         if not email or not name:
             flash('Todos os campos são obrigatórios.', 'error')
-            return render_template('index.html', bootstrap=bootstrap)
+            return render_template('index.html')
 
         # Adiciona os dados à planilha do Google Sheets
         add_to_google_sheet(current_app.config['GOOGLE_CLIENT'], email, name)
@@ -18,4 +16,4 @@ def index(bootstrap):  # Adicione o bootstrap como argumento
         # Redireciona para a URL do documento
         return redirect(current_app.config['URL']) 
 
-    return render_template('index.html', bootstrap=bootstrap)
+    return render_template('index.html')
